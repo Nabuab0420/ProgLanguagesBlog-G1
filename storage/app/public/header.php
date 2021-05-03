f47733033c29a40863862a467f08ead7ec75ef9b
Maan0420 <nabuab_marianne@plpasig.edu.ph>
2021-05-03 19:23:48 +0800

    add files to storage

---
+++ storage/app/public/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<nav class="navbar navbar-light navbar-expand-lg fixed-top bg-white clean-navbar">
+        <div class="container"><a class="navbar-brand logo" href="/home" style="font-family:Aclonica, sans-serif;">GroupONE</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div
+                class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav ml-auto"></ul>
+        </div>
+        </div>
+    </nav>
+</html>

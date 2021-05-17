3691b19f835d24151170fbab7533def612650c28
Nabuab0420 <nabuab_marianne@plpasig.edu.ph>
2021-05-17 22:06:33 +0800

    put all resources

---
+++ resources/views/post1.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Post - Brand</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Aclonica">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
+</head>
+
+<body>
+<?php include 'header.php';?>
+    
+    <main class="page blog-post">
+        <section class="clean-block clean-post dark">
+            <div class="container">
+                <div class="block-content">
+                    <div class="post-image" style="background-image:url(&quot;assets/img/Programming-Languages-scaled.jpg&quot;);"></div>
+                    <div class="post-body">
+                        <h3>Best Programming Languages to Learn in 2021<br><br></h3>
+                        <div class="post-info"><span>By &nbsp;Sruthi Veeraraghavan<br><br></span><span>April 9, 2021<br><br></span></div>
+                        <p class="text-justify" style="font-family:Aclonica, sans-serif;">Once, only a few people were considered computer programmers with cutting-edge coding skills. Now, many IT jobs require a solid grasp of the top programming languages — yes, more than one.&nbsp;If you’re trying to advance in your
+                            career or change careers completely and need to master a programming language, you might wonder which one to learn. After all, it will take time and money to learn the language, so you want to make the right choice.Several
+                            considerations come into play when making your decision, like the difficulty level you’re willing to learn, the knowledge you already possess that align with your existing coding skills, or your reasons for learning a top programming
+                            language.&nbsp;Whether you want to develop a mobile application, get certification for programming knowledge, or learn new skills, you need to learn the right programming language. Below you’ll learn about 10 popular programming
+                            languages that will be in demand among employers in 2021. You’ll find about each language, its complexity, and how it is used.<br><br></p>
+                        <figure class="figure"><img class="figure-img" src="assets/img/Best-Programming-Languages-to-Start-Learning-Today.jpg" style="width:920px;">
+                            <figcaption class="figure-caption"></figcaption>
+                        </figure>
+                        <h4 style="font-family:Aclonica, sans-serif;">1. Python<br></h4>
+                        <p class="text-justify" style="color:rgb(0,0,0);font-family:Aclonica, sans-serif;"><a href="https://www.simplilearn.com/mobile-and-software-development/python-development-training">Python</a>&nbsp;is one of the most commonly used programming languages today and is easy for beginners to learn because of its readability.
+                            It is a free, open-source programming language with extensive support modules and community development, easy integration with web services, user-friendly data structures, and GUI-based desktop applications. It is a popular
+                            programming language for machine learning and deep learning applications.&nbsp;<br><br></p>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <figure class="figure"><img class="figure-img" src="assets/img/ExJCf-TWEAAs7L2.jpg" style="width:420px;"></figure>
+                            </div>
+                            <div class="col">
+                                <p class="text-justify" style="font-family:Aclonica, sans-serif;">Python is used to develop 2D imaging and 3D animation packages like Blender, Inkscape, and Autodesk. It has also been used to create popular video games, including Civilization IV, Vegas Trike, and Toontown. Python is used
+                                    for scientific and computational applications like FreeCAD and Abacus and also by popular websites like YouTube, Quora, Pinterest, and Instagram. Python developers earn&nbsp;<a href="https://www.glassdoor.com/Salaries/python-developer-salary-SRCH_KO0,16.htm">average annual salaries of about $72,500</a>.<br><br></p>
+                            </div>
+                        </div>
+                        <h4 style="font-family:Aclonica, sans-serif;"><br>2. Java<br></h4>
+                        <p class="text-justify" style="font-family:Aclonica, sans-serif;"><a href="https://www.simplilearn.com/mobile-and-software-development/java-javaee-soa-development-training">Java</a>&nbsp;is one of the most common, in-demand computer programming languages used today.&nbsp;Owned by Oracle Corporation,
+                            this general-purpose programming language with its object-oriented structure has become a standard for applications that can be used regardless of platform (e.g., Mac, Windows, Android, iOS, etc.) because of its Write Once,
+                            Run Anywhere (WORA) capabilities. As a result, Java is recognized for its portability across platforms, from mainframe data centers to smartphones. Today there are more than 3 billion devices running applications built with
+                            Java.<br><br></p>
+                        <div class="row">
+                            <div class="col">
+                                <p class="text-justify" style="font-family:Aclonica, sans-serif;">Java is widely used in web and application development as well as big data. Java is also used on the backend of several popular websites, including Google, Amazon, Twitter, and YouTube. It is also extensively used in hundreds
+                                    of applications. New Java frameworks like Spring, Struts, and Hibernate are also very popular. With millions of Java developers worldwide, there are hundreds of ways to learn Java. Also, Java programmers have an extensive
+                                    online community and support each other to solve problems.Java is a more complex language to learn, but experienced developers with Java coding skills are in high-demand. The average Java developer earns around&nbsp;
+                                    <a
+                                        href="https://www.glassdoor.com/Salaries/java-developer-salary-SRCH_KO0,14.htm">$79,000 each year</a>.<br><br></p>
+                            </div>
+                            <div class="col-md-6">
+                                <figure class="figure"><img class="figure-img" src="assets/img/960x0.jpg" style="width:420px;"></figure>
+                            </div>
+                        </div>
+                        <h4 style="font-family:Aclonica, sans-serif;"><br>3. Kotlin<br></h4>
+                        <p class="text-justify" style="font-family:Aclonica, sans-serif;"><br>Kotlin is a general-purpose programming language originally developed and unveiled as Project Kotlin by JetBrains in 2011. The first version was officially released in 2016. It is interoperable with Java and supports functional
+                            programming languages.<br><br><br></p>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <figure class="figure"><img class="figure-img" src="assets/img/kotlin-featured.png" style="width:420px;"></figure>
+                            </div>
+                            <div class="col">
+                                <p class="text-justify" style="font-family:Aclonica, sans-serif;">Kotlin is used extensively for Android apps, web application, desktop application, and server-side application development. Kotlin was built to be better than Java, and people who use this language are convinced. Most of
+                                    the Google applications are based on Kotlin. Some companies using Kotlin as their programming language include Coursera, Pinterest, PostMates among many others.Kotlin developers earn an<a href="https://www.ziprecruiter.com/Salaries/Kotlin-Salary">&nbsp;average of $136,000 a year</a>,
+                                    with the potential to earn up to $171,500.<br><br></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.0/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/js/script.min.js"></script>
+</body>
+
+</html>

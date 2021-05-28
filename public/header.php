e811cb9585d5aef1dbe8b4f05af556bf070a96ed
Nabuab0420 <nabuab_marianne@plpasig.edu.ph>
2021-05-28 22:17:07 +0800

    Update header.php
    Tried putting sessions

---
+++ public/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<nav class="navbar navbar-light navbar-expand-lg fixed-top bg-white clean-navbar">
+        <div class="container"><a class="navbar-brand logo" href="/home" style="font-family:Aclonica, sans-serif;">GroupONE</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div
+                class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav ml-auto"></ul>
+            </div>
+        </div>
+        <div >
+        <div ><a  href="/log" style="font-family:Aclonica, sans-serif;">LogIn</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-3"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div
+                class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav ml-auto"></ul>
+            </div>
+        </div>
+        <div ><a  href="/register" style="font-family:Aclonica, sans-serif;">Register</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-3"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div
+                class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav ml-auto"></ul>
+            </div>
+        </div>
+        </div>
+        <?php
+        //if ($_SESSION["user"] == "")
+        //{
+           // print "<div class="container"><a  href="/log" style="font-family:Aclonica, sans-serif;">Log In</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+           // <div
+             //   class="collapse navbar-collapse" id="navcol-1">
+               // <ul class="nav navbar-nav ml-auto"></ul>
+            //</div>
+            //</div> ";
+
+
+        //}
+        ?>
+    </nav>
+</html>

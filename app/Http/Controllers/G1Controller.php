4d0541a343067d989c38d59567f837111abfc5d7
Nabuab0420 <nabuab_marianne@plpasig.edu.ph>
2021-05-28 19:50:35 +0800

    Update G1Controller.php

---
+++ app/Http/Controllers/G1Controller.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+
+class G1Controller extends Controller
+{
+    public function home(){
+        return view('home');
+    }
+    public function post1(){
+        return view('post1');
+    }
+    public function post2(){
+        return view('post2');
+    }
+    public function post3(){
+        return view('post3');
+    }
+    public function about(){
+        return view('about');
+    }
+    public function login(){
+        return view('login');
+    }
+    public function register(){
+        return view('registration');
+    }
+}
+
+?>
